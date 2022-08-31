09f28357216725d09a4db9408499da3b4f2ad238
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-08-31 15:38:53 +0200

    ns to ms

--- runtimevent/bin/runtimevent.ml
+++ runtimevent/bin/runtimevent.ml
@@ -1,4 +1,5 @@
 open Stack
+
 let rec hanoi depart milieu arrivee = function 
 | 0 -> ()
 | n -> hanoi depart arrivee milieu (n - 1); 
@@ -7,21 +8,18 @@ let rec hanoi depart milieu arrivee = function
 let pile = Stack.create ()
 let list_of_pair = Hashtbl.create 30
 
-let insert_to_hashtable ((k : string), (v : int64 )) (vl : int64 list)= 
+let insert_to_hashtable ((k : string), (v : int64 ))= 
   if Hashtbl.mem list_of_pair k = true then 
     Hashtbl.replace list_of_pair k (v :: (Hashtbl.find list_of_pair k))
   else 
     Hashtbl.add list_of_pair k [v]
 
-let compute (phase : string) (ts2 : int64) (_, ts1) = (phase, (Int64.sub ts2 ts1))
+(* let compute (phase : string) (ts2 : int64) (_, ts1) = 
+  (phase, (Int64.sub ts2 ts1)) *)
 
-(* let rec iter_on_y l = 
-  match l with
-    |[] -> ()
-    |[e] -> 
-      Printf.printf "e de iter on y de la list %Ld \n" e 
-    |h :: tl -> 
-      Printf.printf "h de iter on y de la list%Ld \n" h; iter_on_y tl *)
+let compute (phase2 : string) (ts2 : int64) (phase1, ts1) = 
+  if phase1 = phase2 then (phase2, (Int64.sub ts2 ts1))
+  else failwith "there's no b or e"
 
 let runtime_begin_inter ts phase =
   Stack.push (phase, ts) (pile);;
@@ -31,7 +29,7 @@ let runtime_begin _ ts phase =
 
 let runtime_end _ ts phase =
   insert_to_hashtable (compute (Runtime_events.runtime_phase_name phase)
-    (Runtime_events.Timestamp.to_int64 ts) (Stack.pop (pile))) []
+    (Runtime_events.Timestamp.to_int64 ts) (Stack.pop (pile)))
 
 let () =
   Runtime_events.start ();
@@ -42,17 +40,15 @@ let () =
     ignore(Runtime_events.read_poll cursor callbacks None);
   Unix.sleep 1
 
-let rec sum_of_list (l:int64 list) count =
-  match l with
-  |[] ->  count
-  |h :: tl -> sum_of_list tl (count + (Int64.to_int h)) 
-
-let _ = Hashtbl.iter (fun phase  ts ->
+let _ = Hashtbl.iter (fun phase ts ->
   let len = List.length ts in
-  (* print_string "average of all phase's values :  ";
-  let _ = List.fold_left (fun i count -> (Int64.add i count) ) (Int64.of_int 0) y
+  (* print_int len; 
+  print_string "len \n";
+  print_string "average of all phase's values :  ";
+  let _ = List.fold_left (fun i count -> Printf.printf "i : %Ld \n" i; Printf.printf "count: %Ld \n" (Int64.add i count); 
+    (Int64.add i count) ) (Int64.of_int 0) ts
   in *)
-  (Printf.printf "average of all '%s' values : %d \n%!"
-    phase ((Int64.to_int (List.fold_left
-      (fun i count -> (Int64.add i count) ) (Int64.of_int 0) ts)) / len));
+  (Printf.printf "average of all '%i %s' values : %f ms \n%!"
+    len phase (Int.to_float ((Int64.to_int (List.fold_left
+      (fun i count -> (Int64.add i count) ) (Int64.of_int 0) ts)) / len) *. 0.001));
     )list_of_pair
