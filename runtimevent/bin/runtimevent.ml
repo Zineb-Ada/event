6f7b32ba44aafc1e52a9ed41669f326f40839d5d
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-09-08 00:41:46 +0200

    get phase by name

--- runtimevent/bin/runtimevent.ml
+++ runtimevent/bin/runtimevent.ml
@@ -31,9 +31,9 @@ let compute ev_end =
             if ev_end.phase = ev_begin.phase then Result.ok (Hashtbl.add dict ev_end.phase [diff])
                   else Result.error "the phases are diferente"
           |true -> Result.ok (Hashtbl.replace dict ev_end.phase (diff :: Hashtbl.find dict ev_end.phase)))
-    |true -> Result.error "the stack is empty"
+    |true -> Result.error (Printf.sprintf "the event '%s' is discarded because there's no begin for it" ev_end.phase)  
 
-let ev_end ts phase = {ts= Event.Timestamp.to_int64 ts; phase = Event.runtime_phase_name phase}
+let ev_end ts phase = {ts = Event.Timestamp.to_int64 ts; phase = Event.runtime_phase_name phase}
 
 let runtime_begin _ ts phase=
     Stack.push (ev_end ts phase) pile 
@@ -44,19 +44,24 @@ let runtime_end _ ts phase =
     |Error e -> Printf.printf "%s \n" e
 
 let () =
-  Runtime_events.start ();
-  let cursor = Runtime_events.create_cursor None in
-  let callbacks = Runtime_events.Callbacks.create ~runtime_begin ~runtime_end ()
+  Event.start ();
+  let cursor = Event.create_cursor None in
+  let callbacks = Event.Callbacks.create ~runtime_begin ~runtime_end ()
   in
-    for _ = 0 to 10 do
+      let _ = String.make 1024 'c' in
+      Gc.full_major ();
       hanoi "A" "B" "C" 3;
-      ignore(Runtime_events.read_poll cursor callbacks None);
+      ignore(Event.read_poll cursor callbacks None);
       Unix.sleep 1
-    done
 
-let _ = Hashtbl.iter (fun phase ts ->
-  let len = List.length ts in
-  (Printf.printf "average of all '%i %s' values : %f ms \n%!"
-    len phase (Int.to_float ((Int64.to_int (List.fold_left
-      (fun i count -> (Int64.add i count) ) (Int64.of_int 0) ts)) / len) *. 0.001));
-    )dict
+let get_phases ?phase_name ()=
+  Hashtbl.iter (fun phase ts ->
+    let len = List.length ts in 
+    let float_of_list = (Int.to_float ((Int64.to_int (List.fold_left
+    (fun i count -> (Int64.add i count)) (Int64.of_int 0) ts)) / len) *. 0.001) in 
+      match phase_name with 
+      |None -> Printf.printf "The average of all %s values is %f \n" phase float_of_list
+      |_ -> if phase_name = Some phase then Printf.printf "The average of %i '%s' values is %f \n" len phase float_of_list
+  )dict
+let () = if Array.length Sys.argv > 1 then get_phases ~phase_name:(Sys.argv.(1)) () 
+  else get_phases ()
