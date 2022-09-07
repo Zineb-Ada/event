f987fdff56b1473cfed5d691ec0b4b3eec3273e2
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-09-07 10:55:06 +0200

    exception handling

--- runtimevent/bin/runtimevent.ml
+++ runtimevent/bin/runtimevent.ml
@@ -5,8 +5,13 @@ type event = {
   ts : int64;
   phase : string;
 }
+
 type hash = (string , int64 list) Hashtbl.t
 type stack = event Stack.t
+
+type ('a, 'e) result = 
+  | Ok of 'a
+  | Error of 'e
 
 let rec hanoi depart milieu arrivee = function 
 | 0 -> ()
@@ -16,39 +21,41 @@ let rec hanoi depart milieu arrivee = function
 let dict : hash = Hashtbl.create 30
 let pile : stack = Stack.create ()
 
-let compute ev_end ev_begin=
-  let diff = (Int64.sub ev_end.ts ev_begin.ts) in
-    match Hashtbl.mem dict ev_begin.phase with
-      |false -> if ev_end.phase = ev_begin.phase then Hashtbl.add dict ev_end.phase [diff]
-              else ()
-      |true -> Hashtbl.replace dict ev_end.phase (diff :: Hashtbl.find dict ev_end.phase)
+let compute ev_end =
+  match Stack.is_empty pile with
+    |false ->
+      (let ev_begin = Stack.pop pile in
+      let diff = (Int64.sub ev_end.ts ev_begin.ts) in
+        match Hashtbl.mem dict ev_begin.phase with
+          |false -> 
+            if ev_end.phase = ev_begin.phase then Result.ok (Hashtbl.add dict ev_end.phase [diff])
+                  else Result.error "the phases are diferente"
+          |true -> Result.ok (Hashtbl.replace dict ev_end.phase (diff :: Hashtbl.find dict ev_end.phase)))
+    |true -> Result.error "the stack is empty"
 
 let ev_end ts phase = {ts= Event.Timestamp.to_int64 ts; phase = Event.runtime_phase_name phase}
 
 let runtime_begin _ ts phase=
-    Stack.push (ev_end ts phase) pile
+    Stack.push (ev_end ts phase) pile 
 
 let runtime_end _ ts phase =
-  compute (ev_end ts phase) (Stack.pop pile)
+  match compute (ev_end ts phase) with
+    |Ok v -> ()
+    |Error e -> Printf.printf "%s \n" e
 
-let () =
+let _ =
   Runtime_events.start ();
   let cursor = Runtime_events.create_cursor None in
   let callbacks = Runtime_events.Callbacks.create ~runtime_begin ~runtime_end ()
   in
-    hanoi "A" "B" "C" 3;
-    ignore(Runtime_events.read_poll cursor callbacks None);
-  Unix.sleep 1
+    for _ = 0 to 10 do
+      hanoi "A" "B" "C" 3;
+      ignore(Runtime_events.read_poll cursor callbacks None);
+      Unix.sleep 1
+    done
 
 let _ = Hashtbl.iter (fun phase ts ->
   let len = List.length ts in
-  print_int len;
-  (* print_int len; 
-  print_string "len \n";
-  print_string "average of all phase's values :  ";
-  let _ = List.fold_left (fun i count -> Printf.printf "i : %Ld \n" i; Printf.printf "count: %Ld \n" (Int64.add i count); 
-    (Int64.add i count) ) (Int64.of_int 0) ts
-  in *)
   (Printf.printf "average of all '%i %s' values : %f ms \n%!"
     len phase (Int.to_float ((Int64.to_int (List.fold_left
       (fun i count -> (Int64.add i count) ) (Int64.of_int 0) ts)) / len) *. 0.001));
