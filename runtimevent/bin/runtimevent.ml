e4de51cf43b2aeee85f506886797d4c832d13ae1
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-09-08 14:16:43 +0200

    counter handling

--- runtimevent/bin/runtimevent.ml
+++ runtimevent/bin/runtimevent.ml
@@ -6,7 +6,13 @@ type event = {
   phase : string;
 }
 
+type event_counter = {
+  tis : int;
+  counter_name : string;
+}
+
 type hash = (string , int64 list) Hashtbl.t
+type counter_hash = (string , int list) Hashtbl.t
 type stack = event Stack.t
 
 type ('a, 'e) result = 
@@ -19,6 +25,7 @@ let rec hanoi depart milieu arrivee = function
         hanoi milieu depart arrivee (n - 1)
 
 let dict : hash = Hashtbl.create 30
+let counter_dict : counter_hash = Hashtbl.create 30
 let pile : stack = Stack.create ()
 
 let compute ev_end =
@@ -34,6 +41,7 @@ let compute ev_end =
     |true -> Result.error (Printf.sprintf "the event '%s' is discarded because there's no begin for it" ev_end.phase)  
 
 let ev_end ts phase = {ts = Event.Timestamp.to_int64 ts; phase = Event.runtime_phase_name phase}
+let ev_counter tis counter_name = {tis = Int64.to_int(Event.Timestamp.to_int64 tis); counter_name= Event.runtime_counter_name counter_name}
 
 let runtime_begin _ ts phase=
     Stack.push (ev_end ts phase) pile 
@@ -43,10 +51,16 @@ let runtime_end _ ts phase =
     |Ok v -> ()
     |Error e -> Printf.printf "%s \n" e
 
+let runtime_counter _ tis counter_name _ = 
+  match Hashtbl.mem counter_dict (Event.runtime_counter_name counter_name) with
+    |false -> Hashtbl.add counter_dict (Event.runtime_counter_name counter_name) [Int64.to_int(Event.Timestamp.to_int64 tis)]
+    |true -> Hashtbl.replace counter_dict (Event.runtime_counter_name counter_name) 
+      (Int64.to_int(Event.Timestamp.to_int64 tis) :: Hashtbl.find counter_dict (Event.runtime_counter_name counter_name))
+
 let () =
   Event.start ();
   let cursor = Event.create_cursor None in
-  let callbacks = Event.Callbacks.create ~runtime_begin ~runtime_end ()
+  let callbacks = Event.Callbacks.create ~runtime_begin ~runtime_end ~runtime_counter ()
   in
       let _ = String.make 1024 'c' in
       Gc.full_major ();
@@ -55,13 +69,18 @@ let () =
       Unix.sleep 1
 
 let get_phases ?phase_name ()=
+  Hashtbl.iter (fun counter_name tis ->
+    let len = List.length tis in 
+    if counter_name = "minor_allocated" then Printf.printf "number of counters for '%s' is %d \n" counter_name len
+  )counter_dict;
   Hashtbl.iter (fun phase ts ->
-    let len = List.length ts in 
+    let len = List.length ts in
     let float_of_list = (Int.to_float ((Int64.to_int (List.fold_left
-    (fun i count -> (Int64.add i count)) (Int64.of_int 0) ts)) / len) *. 0.001) in 
-      match phase_name with 
+    (fun i count -> (Int64.add i count)) (Int64.of_int 0) ts)) / len) *. 0.001) in
+      match phase_name with
       |None -> Printf.printf "The average of all %s values is %f \n" phase float_of_list
       |_ -> if phase_name = Some phase then Printf.printf "The average of %i '%s' values is %f \n" len phase float_of_list
   )dict
-let () = if Array.length Sys.argv > 1 then get_phases ~phase_name:(Sys.argv.(1)) () 
+let () = 
+  if Array.length Sys.argv > 1 then get_phases ~phase_name:(Sys.argv.(1)) () 
   else get_phases ()
