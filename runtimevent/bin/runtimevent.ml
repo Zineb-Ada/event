d66048adf413efae9ba7ddbe2b6015084e735edb
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-10-17 15:17:31 +0200

    hh

--- runtimevent/bin/runtimevent.ml
+++ runtimevent/bin/runtimevent.ml
@@ -1,24 +1,101 @@
-open Counter
 open Phase
+open Counter
+
+(* open Cmdliner *)
 module Event = Runtime_events
 
-let rec hanoi depart milieu arrivee = function 
+(* let rec hanoi depart milieu arrivee = function 
 | 0 -> ()
 | n -> hanoi depart arrivee milieu (n - 1); 
-        hanoi milieu depart arrivee (n - 1)
+        hanoi milieu depart arrivee (n - 1) *)
+
+let bind o f =
+  match o with
+  | Some x  -> f x
+  | None    -> None
+
+let (let*) x f = bind x f
+
+let timeout =
+    let* p = Some (Lwt_unix.sleep 10.) in
+    Some (Lwt.return None)
+
 let _ =
   Event.start ();
+  (* Event.dir="/Users/tarides/Desktop/Tarides-Ocaml/ppxlib"; *)
+  (* Event.dir="/Users/tarides/Desktop/Tarides-Ocaml/irmin"; *)
   let cursor = Event.create_cursor None in
   let callbacks = Event.Callbacks.create ~runtime_begin ~runtime_end ~runtime_counter ()
   in
     let _ = String.make 1024 'c' in
     Gc.full_major ();
-    hanoi "A" "B" "C" 3;
+    (* Event.pause ();
+    let _ = timeout in
+    (* Lwt_unix.timeout 5.0; *)
+    Event.start (); *)
+    Gc.full_major ();
+    (* faire une boucle qui va aretter le programme un petit moment et le relance *)
+    (* hanoi "A" "B" "C" 3; *)
     ignore(Event.read_poll cursor callbacks None);
     Unix.sleep 1;
     if Array.length Sys.argv = 2  then get_phases ~phase_name:(Sys.argv.(1)) ()
     else if Array.length Sys.argv > 2 then get_phases ~phase_name:(Sys.argv.(1)) ~counter_name_input:(Sys.argv.(2)) ()
     else get_phases ()
+
+(* let get_elements phases counters = get_phases (get_counter ())
+let count =
+  let doc = "get phases $(docv)" in
+  Arg.(value & opt string "" & info ["p"; "phases"] ~docv:"PHASES" ~doc)
+let msg =
+  (* let env = *)
+    let doc = "Overrides the default message to print." in
+    (* Cmd.Env.info "ELEMENTS_MSG" ~doc
+  in
+  let doc = "The message to print." in *)
+  Arg.(value & opt (some string) None "" & info ["c", "counters"] ~docv:"COUNTERS" ~doc)
+
+let get_elements_t = Term.(const get_elements $ count $ msg)
+let cmd =
+  let doc = "print elements" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <bugs@example.org>." ]
+  in
+  let info = Cmd.info "get_elements" ~version:"%‌%VERSION%%" ~doc ~man in
+  Cmd.v info get_elements_t
+
+let main () = exit (Cmd.eval cmd)
+let () = main () *)
+
+
+(* let chorus count msg = for _ = 1 to count do print_endline msg done
+let count =
+  let doc = "Repeat the message $(docv) times." in
+  Arg.(value & opt int 10 & info ["c"; "count"] ~docv:"COUNT" ~doc)
+let msg =
+  let env =
+    let doc = "Overrides the default message to print." in
+    Cmd.Env.info "CHORUS_MSG" ~doc
+  in
+  let doc = "The message to print." in
+  Arg.(value & pos 0 string "Revolt!" & info [] ~env ~docv:"MSG" ~doc)
+
+let chorus_t = Term.(const chorus $ count $ msg)
+let cmd =
+  let doc = "print a customizable message repeatedly" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "Email bug reports to <bugs@example.org>." ]
+  in
+  let info = Cmd.info "chorus" ~version:"%‌%VERSION%%" ~doc ~man in
+  Cmd.v info chorus_t
+
+let main () = exit (Cmd.eval cmd)
+let () = main () *)
+
+
+
+(* command + -phase ou -counter +optionnal phase *)
 
 (* let () = 
   (* match Array.length Sys.argv with
--- runtimevent/lib/counter/counter.ml
+++ runtimevent/lib/counter/counter.ml
@@ -1,9 +1,5 @@
+(* open Core.Std *)
 module Event = Runtime_events
-
-(* type event_counter = {
-  tis : int;
-  counter_name : string;
-} *)
 
 type counter_hash = (string , int list) Hashtbl.t
 
@@ -17,9 +13,30 @@ let runtime_counter _ tis counter_name _ =
 
 let get_counter ?counter_name_input ()=
   Hashtbl.iter (fun counter_name tis ->
+    let ltis = tis in
     let len = List.length tis in 
     match counter_name_input with
-    |None -> Printf.printf "number of counters for '%s' is %d \n" counter_name len
-    |_ -> if (Some counter_name) = (counter_name_input) 
-      then Printf.printf "number of counters for '%s' is %d \n" counter_name len
+      (* |None -> List.iter (fun c -> Printf.printf "%s %d \n" counter_name c) tis *)
+      (* let rec  *)
+      (* |None -> match tis with  *)
+        (* | [] ->  *)
+      | None -> (let _ = Printf.printf "%s " counter_name in 
+        (* let tl = [] in *)
+        let rec loop l = 
+          match l with 
+            | [] -> () 
+            | [e] -> Printf.printf "%d \n" e
+            | h :: t -> if h = List.hd ltis then (Printf.printf "%d " h; loop t) else (Printf.printf "%d " h; loop t) in
+        loop tis) 
+        (* List.iter (fun c -> Printf.printf " %d \n"  c) tis *)
+      (* Printf.sprintf "%s %i" counter_name tis *)
+      (* List.iter (fun c -> Printf.printf "%s %d \n" counter_name c) tis *)
+
+      (* Printf.printf "number of counters for '%s' is %d \n" counter_name len *)
+      |_ -> 
+      (* Printf.sprintf "%s %d" counter_name len *)
+      if (Some counter_name) = (counter_name_input) 
+        then Printf.printf "number of counters for '%s' is %d \n" counter_name len
   )counter_dict
+
+  (* let () = Out_channel.write_all "your_file.txt" ~data:"Your text" *)
