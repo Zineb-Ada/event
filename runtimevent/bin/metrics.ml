c7e1617c74e67dfa0d04cfee6dd899902344d417
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-10-18 17:53:45 +0200

    Update metrics.ml

--- runtimevent/bin/metrics.ml
+++ runtimevent/bin/metrics.ml
@@ -5,6 +5,7 @@ let subsystem = "main"
 module Counters = struct
   open Prometheus
 
+  
 let minor_promoted =
   let help = "Number of words promoted to the shared heap" in
   Counter.v ~help ~namespace ~subsystem "minor_promoted"
